8d14ce59af0ea2e096aa302c28a51a946c5c1c69
zahraAfa <anisafatimahazzahra@gmail.com>
2023-07-01 06:33:17 +0700

    custom splash screen has been added

--- lib/src/constants/images.dart
+++ lib/src/constants/images.dart
@@ -2,4 +2,6 @@ class DefaultImages {
   static const String o1 = 'assets/Onboarding-1.png';
   static const String o2 = 'assets/Onboarding-2.png';
   static const String o3 = 'assets/Onboarding-3.png';
+  static const String splashLogo = 'assets/logos/splash-logo.png';
+  static const String mainLogo = 'assets/logos/main-logo.png';
 }
--- lib/src/screens/auth/splash_screen.dart
+++ lib/src/screens/auth/splash_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:sneakerhub_app/src/constants/colors.dart';
+import 'package:sneakerhub_app/src/constants/images.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ConstColors.blackColor,
+      body: Center(
+        child: Image.asset(DefaultImages.splashLogo),
+      ),
+    );
   }
 }
